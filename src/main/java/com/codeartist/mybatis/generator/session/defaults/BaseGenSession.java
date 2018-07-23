f0a3288283ae17617dd298f3d533948f46672233
AiJiangnan <904629998@qq.com>
2018-07-23 20:07:48 +0800

    [update] add ThreadPool to GenSession

--- src/main/java/com/codeartist/mybatis/generator/session/defaults/BaseGenSession.java
+++ src/main/java/com/codeartist/mybatis/generator/session/defaults/BaseGenSession.java
@@ -5,6 +5,9 @@ import com.codeartist.mybatis.generator.executor.BaseExecutor;
 import com.codeartist.mybatis.generator.executor.Executor;
 import com.codeartist.mybatis.generator.session.GenSession;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BaseGenSession implements GenSession {
 
     private final Executor executor;
@@ -15,9 +18,11 @@ public class BaseGenSession implements GenSession {
 
     @Override
     public void generate() {
-        generateModel();
-        generateDao();
-        generateMapper();
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.execute(this::generateModel);
+        executor.execute(this::generateDao);
+        executor.execute(this::generateMapper);
+        executor.shutdown();
     }
 
     @Override
